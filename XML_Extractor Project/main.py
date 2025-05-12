dd27562c8cd2a869d9f6ca70c082c82bd481a7f5
olRenz <124468968+olRenz@users.noreply.github.com>
2025-05-12 16:40:37 -0700

    Update main.py

--- XML_Extractor Project/main.py
+++ XML_Extractor Project/main.py
@@ -1,13 +1,20 @@
 from Extractor import Extractor
 from xml_maker import xml_maker
+from weapon_maker import weapon_maker
+
+# IDEA make the input different folders that I manually set. 
+# then the outputs will be correct and run a different function depending on the folder they open
 
 def my_function():
-  print("Function called")
-  xml_data = Extractor().get_dict()
-  xml_func = xml_maker(xml_data)
+	print("Function called")
+	xml_data = Extractor().get_dict()
+	weapon_data = Extractor().pull_weapon_data()
+	xml_maker(xml_data)
+	weapon_maker(weapon_data)
 
 if __name__ == "__main__":
-  print("Script executed directly")
-  my_function()
+	print("Script executed directly")
+	my_function()
+
 else:
-  print("Script imported as a module")
+	print("Script imported as a module")
